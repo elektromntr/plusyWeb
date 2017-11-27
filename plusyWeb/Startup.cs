48f38dc9ae720d4fd6b8a0e844f044bebe5836d8
elektromntr <bartosztadeuszwojcik@gmail.com>
2017-11-27 22:47:41 +0100

    Add project files.

---
+++ plusyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(plusyWeb.Startup))]
+namespace plusyWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
